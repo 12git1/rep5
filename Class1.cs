c98cae73a05466f60ca994987be1e9d18bfe4930
Sergey Perel <sergeyperel0@gmail.com>
2024-11-11 22:13:05 +0500

    Второй коммит

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5
+{
+    class Class1
+    {
+        private int a;
+        private char b;
+        public Class1(int A, char B)
+        {
+            a = A;
+            b = B;
+        }
+    }
+}
